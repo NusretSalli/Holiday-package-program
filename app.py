4acf35ad04c6c5c3fa89c1f8270f203677676041
NusretSalli <nusret.emirhan.salli@gmail.com>
2022-12-18 00:23:37 +0100

    Website

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/login")
+
+def login():
+
+    return render_template("login.html")
+
+
+@app.route("/packages")
+
+def packages():
+
+    return render_template("packages.html")
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
+
