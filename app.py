06cc54c84f67b59ebdf343a29894028f1cbd474f
NusretSalli <nusret.emirhan.salli@gmail.com>
2022-12-18 10:42:21 +0100

    website

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime
 
 app = Flask(__name__)
 
