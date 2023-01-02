86bfbd8e358316aa06209e06a48ef0f918c1ce6c
NusretSalli <nusret.emirhan.salli@gmail.com>
2023-01-02 13:11:15 +0100

    APP UPDATED

--- APP/app.py
+++ APP/app.py
@@ -17,16 +17,9 @@ def hello_world():
 def login():
 
     if request.method == 'POST':
-        if request.form.get('action1') == 'VALUE1':
-            pass # do something
+
+        placeholder = 2
         
-        elif  request.form.get('action2') == 'VALUE2':
-            pass # do something else
-        
-        else:
-            pass # unknown
-        
-    elif request.method == 'GET':
         return render_template('login.html')
 
     return render_template("login.html")
